35681a4de789a5f868c9679b253d1e545a1d8da6
Erlendum <erlendum.ds@gmail.com>
2023-08-06 11:36:44 +0300

    100

---
+++ 100/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func createNode(val int) *TreeNode {
+	node := new(TreeNode)
+	node.Val = val
+	node.Left = nil
+	node.Right = nil
+	return node
+}
+
+func createTree(data []string) *TreeNode {
+	rootVal, err := strconv.Atoi(data[0])
+	if err != nil {
+		return nil
+	}
+	root := createNode(rootVal)
+
+	curNodes := make([]*TreeNode, 0)
+	curNodes = append(curNodes, root)
+	for i := 1; i < len(data); i += 2 * i {
+		for j := i; j <= 2*i; j++ {
+			var node *TreeNode
+			if data[j] != "null" {
+				nodeVal, err := strconv.Atoi(data[j])
+				if err != nil {
+					return nil
+				}
+				node = createNode(nodeVal)
+			}
+			if j%2 == 1 {
+				if curNodes[0] != nil {
+					curNodes[0].Left = node
+				}
+			} else {
+				if curNodes[0] != nil {
+					curNodes[0].Right = node
+				}
+				curNodes = curNodes[1:]
+			}
+			curNodes = append(curNodes, node)
+
+		}
+	}
+	return root
+}
+
+func Print(root *TreeNode) {
+	if root == nil {
+		fmt.Print("null ")
+		return
+	}
+	Print(root.Left)
+	Print(root.Right)
+	fmt.Printf("%d ", root.Val)
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	} else if p != nil && q != nil {
+		if p.Val != q.Val {
+			return false
+		}
+	} else {
+		return false
+	}
+	if !isSameTree(p.Left, q.Left) {
+		return false
+	}
+	if !isSameTree(p.Right, q.Right) {
+		return false
+	}
+	return true
+}
+
+func main() {
+	t1 := createTree([]string{"1", "2", "3"})
+	t2 := createTree([]string{"1", "2", "3"})
+	Print(t1)
+	fmt.Println()
+	Print(t2)
+	fmt.Println()
+
+	fmt.Println(isSameTree(t1, t2))
+}

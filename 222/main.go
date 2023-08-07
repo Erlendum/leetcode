bec7a7d988c2debee41fa7c084b613fdb2cd8651
Erlendum <erlendum.ds@gmail.com>
2023-08-07 11:29:03 +0300

    222

---
+++ 222/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func createNode(val int) *TreeNode {
+	node := new(TreeNode)
+	node.Val = val
+	node.Left = nil
+	node.Right = nil
+	return node
+}
+
+func createTree(data []string) *TreeNode {
+	rootVal, err := strconv.Atoi(data[0])
+	if err != nil {
+		return nil
+	}
+	root := createNode(rootVal)
+
+	curNodes := make([]*TreeNode, 0)
+	curNodes = append(curNodes, root)
+	for i := 1; i < len(data); i += 2 * i {
+		for j := i; j <= 2*i; j++ {
+			var node *TreeNode
+			if data[j] != "null" {
+				nodeVal, err := strconv.Atoi(data[j])
+				if err != nil {
+					return nil
+				}
+				node = createNode(nodeVal)
+			}
+			if j%2 == 1 {
+				if curNodes[0] != nil {
+					curNodes[0].Left = node
+				}
+			} else {
+				if curNodes[0] != nil {
+					curNodes[0].Right = node
+				}
+				curNodes = curNodes[1:]
+			}
+			curNodes = append(curNodes, node)
+
+		}
+	}
+	return root
+}
+
+func Print(root *TreeNode) {
+	if root == nil {
+		fmt.Print("null ")
+		return
+	}
+	Print(root.Left)
+	Print(root.Right)
+	fmt.Printf("%d ", root.Val)
+}
+
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return 1 + countNodes(root.Left) + countNodes(root.Right)
+}
+
+func main() {
+	tree := createTree([]string{"1", "2", "3", "4", "5", "6", "null"})
+	Print(tree)
+	fmt.Println()
+	fmt.Println(countNodes(tree))
+}

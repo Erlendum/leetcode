892faf83f3391d13841630f8bacfb3806c6365ee
Erlendum <erlendum.ds@gmail.com>
2023-08-13 11:24:21 +0300

    108

---
+++ 108/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func createNode(val int) *TreeNode {
+	node := new(TreeNode)
+	node.Val = val
+	node.Left = nil
+	node.Right = nil
+	return node
+}
+
+func createTree(data []string) *TreeNode {
+	rootVal, err := strconv.Atoi(data[0])
+	if err != nil {
+		return nil
+	}
+	root := createNode(rootVal)
+
+	curNodes := make([]*TreeNode, 0)
+	curNodes = append(curNodes, root)
+	for i := 1; i < len(data); i += 2 * i {
+		for j := i; j < len(data) && j <= 2*i; j++ {
+			var node *TreeNode
+			if data[j] != "null" {
+				nodeVal, err := strconv.Atoi(data[j])
+				if err != nil {
+					return nil
+				}
+				node = createNode(nodeVal)
+			}
+			if j%2 == 1 {
+				if curNodes[0] != nil {
+					curNodes[0].Left = node
+				}
+			} else {
+				if curNodes[0] != nil {
+					curNodes[0].Right = node
+				}
+				curNodes = curNodes[1:]
+			}
+			curNodes = append(curNodes, node)
+
+		}
+	}
+	return root
+}
+
+func Print(root *TreeNode) {
+	if root == nil {
+		fmt.Print("null ")
+		return
+	}
+	fmt.Printf("%d ", root.Val)
+	Print(root.Left)
+	Print(root.Right)
+}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	mid := len(nums) / 2
+	root := TreeNode{Val: nums[mid], Left: sortedArrayToBST(nums[:mid]), Right: sortedArrayToBST(nums[mid+1:])}
+	return &root
+}

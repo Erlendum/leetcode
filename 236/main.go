faaf2dd963402a5b5330449d5a3a96f8e965a4b5
Erlendum <erlendum.ds@gmail.com>
2023-08-15 10:14:10 +0300

    236

---
+++ 236/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func createNode(val int) *TreeNode {
+	node := new(TreeNode)
+	node.Val = val
+	node.Left = nil
+	node.Right = nil
+	return node
+}
+
+func createTree(data []string) *TreeNode {
+	rootVal, err := strconv.Atoi(data[0])
+	if err != nil {
+		return nil
+	}
+	root := createNode(rootVal)
+
+	curNodes := make([]*TreeNode, 0)
+	curNodes = append(curNodes, root)
+	for i := 1; i < len(data); i += 2 * i {
+		for j := i; j <= 2*i; j++ {
+			var node *TreeNode
+			if data[j] != "null" {
+				nodeVal, err := strconv.Atoi(data[j])
+				if err != nil {
+					return nil
+				}
+				node = createNode(nodeVal)
+			}
+			if j%2 == 1 {
+				if curNodes[0] != nil {
+					curNodes[0].Left = node
+				}
+			} else {
+				if curNodes[0] != nil {
+					curNodes[0].Right = node
+				}
+				curNodes = curNodes[1:]
+			}
+			curNodes = append(curNodes, node)
+
+		}
+	}
+	return root
+}
+
+func Print(root *TreeNode) {
+	if root == nil {
+		fmt.Print("null ")
+		return
+	}
+	fmt.Printf("%d ", root.Val)
+	Print(root.Left)
+	Print(root.Right)
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root == p || root == q {
+		return root
+	}
+
+	l := lowestCommonAncestor(root.Left, p, q)
+	r := lowestCommonAncestor(root.Right, p, q)
+
+	if l != nil && r != nil {
+		return root
+	}
+
+	if l == nil {
+		return r
+	}
+	return l
+}
+
+func main() {
+	tree := createTree([]string{"3", "5", "1", "6", "2", "0", "8"})
+	Print(tree)
+	fmt.Println()
+	fmt.Println(lowestCommonAncestor(tree, tree.Left.Right, tree.Right.Right).Val)
+}

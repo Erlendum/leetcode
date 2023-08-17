8067824ec04f6a1b6acbd0fbd667f05c4cc1d9df
Erlendum <erlendum.ds@gmail.com>
2023-08-17 11:39:28 +0300

    46

---
+++ 46/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func permute(nums []int) [][]int {
+	var helper func([]int, int)
+	var res [][]int
+
+	helper = func(arr []int, n int) {
+		if n == 1 {
+			tmp := make([]int, len(arr))
+			copy(tmp, arr)
+			res = append(res, tmp)
+		} else {
+			for i := 0; i < n; i++ {
+				helper(arr, n-1)
+				if n%2 == 1 {
+					tmp := arr[i]
+					arr[i] = arr[n-1]
+					arr[n-1] = tmp
+				} else {
+					tmp := arr[0]
+					arr[0] = arr[n-1]
+					arr[n-1] = tmp
+				}
+			}
+		}
+	}
+	helper(nums, len(nums))
+	return res
+}
+
+func main() {
+	nums := []int{1, 2, 3}
+	fmt.Println(permute(nums))
+}
